0d0eb8cde8bcf69df73568660222b0dc6e8fff86
Spoorthi Samala <spoorthisamala@gmail.com>
2019-07-12 10:22:54 +0530

    Spring Task 5

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,11 +1,13 @@
 package com.stackroute;
 
+import com.stackroute.domain.BeanLifecycleDemoBean;
 import com.stackroute.domain.Movie;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.ResolvableType;
@@ -15,10 +17,12 @@ import org.springframework.core.io.FileSystemResource;
 
 public class Main {
     public static void main(String args[]){
-        ApplicationContext context=new ClassPathXmlApplicationContext("file:src/beans.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("file:src/beans.xml");
         BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("src/beans.xml"));
         Movie movie=context.getBean("movie",Movie.class);
         movie.display();
+        BeanLifecycleDemoBean beanLifecycleDemoBean=context.getBean("beanlifecycle",BeanLifecycleDemoBean.class);
+        context.close();
 
     }
 }
---
+++ src/main/java/com/stackroute/domain/BeanLifecycleDemoBean.java
@@ -0,0 +1,21 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
+
+    public void customInit(){
+        System.out.println("Init");
+    }
+    public void customDestroy(){
+        System.out.println("Destroy");
+    }
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initialization method called");
+    }
+        public void destroy() throws Exception {
+            System.out.println("destroy method called");
+        }
+    }
+
